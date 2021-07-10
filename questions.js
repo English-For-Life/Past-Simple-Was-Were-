dc5da50b3b275ff4dd506b4734691b6391836866
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-07-10 12:26:19 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"There _____ any students on the campus yesterday because they were away for a holiday.",
+        options:["weren't", "wasn't"],
+        answer: 0
+    },
+    {
+        q:"I _____ in Germany last month and people at the hotel _____ very friendly.",
+        options:["was / was", "was / were"],
+        answer: 1
+    },
+    {
+        q:"The films at the last summer's festival _____ quite good, so he _____ happy to watch them.",
+        options:["was / was", "were / was"],
+        answer: 1
+    },
+    {
+        q:"Nick _____ alone yesterday. He _____ with his family in the city.",
+        options:["wasn't / was", "was / was"],
+        answer: 0
+    },
+    {
+        q:"That museum _____ a house in the past and there _____ some apple trees in its garden then.",
+        options:["were / was", "was / were"],
+        answer: 1
+    },
+    {
+        q:"There _____ trees on that mountain five years ago.",
+        options:["were", "was"],
+        answer: 0
+    },
+    {
+        q:"There _____ even one candle on his birthday cake.",
+        options:["weren't", "wasn't"],
+        answer: 1
+    },
+    {
+        q:"Daniel and I _____ both ill yesterday.",
+        options:["were", "was"],
+        answer: 0
+    },
+    {
+        q:"_____ the music at the party good last night? - Some songs _____ superb.",
+        options:["Were / was", "Was / were"],
+        answer: 1
+    },
+    {
+        q:"Where _____ you 30 minutes ago? - I _____ at school.",
+        options:["were / was", "was / were"],
+        answer: 0
+    }
+
+
+
+]
